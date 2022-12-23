02331477fde6ee3ec2ba5109ae572895a6fb8766
Tyler Neu <tyler.neu@ambition.com>
2022-12-23 13:01:35 -0600

    Max string length. Colored filter output.

--- filter.py
+++ filter.py
@@ -1,10 +1,11 @@
-# 
-
 import requests
 from ics import Calendar
 from datetime import datetime, timedelta
 import pytz
 import re
+from rich.console import Console
+
+console = Console()
 
 # FILTER OPTIONS
 
@@ -26,6 +27,8 @@ EXCLUDE_MULTI_DAY = True
 
 EXCLUDED_IDS = []
 FORCE_INCLUDED_IDS = []
+
+MAX_NAME_LENGTH = 30
 
 r = requests.get(
     'https://scoutbook.scouting.org/ics/44935.D37B9.ics',
@@ -65,7 +68,16 @@ for e in sorted(c.events):
             append = False
 
     if append or force_append:
+
+        if len(e.name) > MAX_NAME_LENGTH:
+            e.name = e.name[0:MAX_NAME_LENGTH] 
+            console.print(e.begin.strftime('%m/%d/%y'), e.name, style="black on yellow")
+        else:
+            console.print(e.begin.strftime('%m/%d/%y'), e.name, style="white on green")
+
         events.append(e)
+    else:
+        console.print(e.begin.strftime('%m/%d/%y'), e.name, style="white on red")
 
 
 new_c = Calendar(events=events)
