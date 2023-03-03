09a04036e63c86237835d93fb51ab9d60cfd3e61
Tyler Neu <tyler.neu@ambition.com>
2023-03-02 22:08:29 -0600

    WIP

--- filter.py
+++ filter.py
@@ -3,44 +3,48 @@ from ics import Calendar
 from datetime import datetime, timedelta
 import pytz
 import re
-# from rich.console import Console
+from rich.console import Console
 
-# console = Console()
+console = Console()
 
 configs = [
-    {
-        'start_days': 10,
-        'end_days': 60,
-        'exclude_regex': [
-            "break",
-            "no school",
-            "Roundtable",
-        ],
-        'remove_substring': [],
-        'include_regex': [],
-        'exclude_multi_day': False,
-        'excluded_ids': [],
-        'force_included_ids': [],
-        'max_name_length': 30,
-        'add_time_zone': False,
-        'url': 'https://scoutbook.scouting.org/ics/44935.D37B9.ics',
-        'outgoing_filename': 'filtered_troop_150.ics',
-    },
+    # {
+    #     'start_days': 10,
+    #     'end_days': 60,
+    #     'exclude_regex': [
+    #         "break",
+    #         "no school",
+    #         "Roundtable",
+    #     ],
+    #     'remove_substring': [],
+    #     'include_regex': [],
+    #     'exclude_multi_day': False,
+    #     'excluded_ids': [],
+    #     'force_included_ids': [],
+    #     'max_name_length': 30,
+    #     'add_time_zone': False,
+    #     'url': 'https://scoutbook.scouting.org/ics/44935.D37B9.ics',
+    #     'outgoing_filename': 'filtered_troop_150.ics',
+    # },
     {
         'start_days': 10,
         'end_days': 120,
         'exclude_regex': [],
+        'append_description': True,
         'remove_substring': [
-            ' - Spring 2023 Practice'
+            ' - Spring 2023 Practice',
+            ' - SPRING 2023',
+            'PW8BB ',
+            '\.'
         ],
         'include_regex': [],
         'exclude_multi_day': True,
         'excluded_ids': [],
         'force_included_ids': [],
-        'max_name_length': 30,
+        # 'max_name_length': 30,
         'add_time_zone': True,
         'url': 'https://sportsplus.app/my/sports-life/schedule/member/icalendar-subscribe/81807/MySchedule.ics',
-        'outgoing_filename': 'braves_spring_2023.ics'
+        'outgoing_filename': 'braves_spring_2023.ics',
     }
 ]
 
@@ -60,6 +64,10 @@ for config in configs:
     for e in sorted(c.events):
         append = True
         force_append = False
+
+        if config['append_description']:
+            if e.description:
+                e.name = f'{e.name} - {e.description}'
 
         if not (e.begin > START_DATE and e.begin < END_DATE):
             append = False
@@ -89,10 +97,11 @@ for config in configs:
         if append or force_append:
 
             for substring in config['remove_substring']:
-                e.name = e.name.replace(substring, '')
+                # e.name = e.name.replace(substring, '')
+                e.name = re.sub(substring, '', e.name)
                 # console.print(e.begin.strftime('%m/%d/%y'), e.name, style="black on yellow")
 
-            if len(e.name) > config['max_name_length']:
+            if 'max_name_length' in config and len(e.name) > config['max_name_length']:
                 e.name = e.name[0:config['max_name_length']]
                 # console.print(e.begin.strftime('%m/%d/%y'), e.name, style="black on yellow")
             else:
@@ -103,10 +112,11 @@ for config in configs:
                 e.end = e.end.replace(tzinfo=pytz.timezone('US/Central'))
                 e.begin = e.begin.replace(tzinfo=pytz.timezone('US/Central'))
 
+            console.print(e.begin.strftime('%m/%d/%y'), e.name)
             events.append(e)
         else:
-            pass
             # console.print(e.begin.strftime('%m/%d/%y'), e.name, style="white on red")
+            pass
 
 
     new_c = Calendar(events=events)
